2870279a065dfedcd447cabd0eb0760ca8f3c712
Ömer Aziz ŞAHİN <omarazizsah@gmail.com>
2025-02-05 22:31:57 +0300

    feat: pinia init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  modules: ['@pinia/nuxt'],
   devtools: {
     enabled: true,
 
     timeline: {
-      enabled: true
-    }
-  }
-})
+      enabled: true,
+    },
+  },
+});
