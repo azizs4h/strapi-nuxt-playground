ac38801bbf78c7f82785e1ab5bf72e6a34f5581a
Ömer Aziz ŞAHİN <omarazizsah@gmail.com>
2025-02-05 22:36:31 +0300

    feat: pinia-plugin-persistedstate/nuxt init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt'],
   devtools: {
     enabled: true,
 
